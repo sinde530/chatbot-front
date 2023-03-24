72ba3c327d48930d864b573d47d1ccccb7c10e59
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-03-24 17:30:31 +0900

    fixed design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import styled from '@emotion/styled';
+
 import Chat from './component/Chat';
+
+export const Container = styled.div({
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    background: 'red',
+    alignItems: 'center',
+    margin: '0 auto',
+});
 
 export default function App() {
     return (
-        <div>
+        <Container>
             <Chat />
-        </div>
+        </Container>
     );
 }
